afdd15664d11b6c2e66d31403f714da095b88f31
Michelle <michelleteixeiramartins@gmail.com>
2022-02-17 14:38:02 +0000

    contrato

--- contrato/Pokcard.sol
+++ contrato/Pokcard.sol
@@ -1,11 +1,7 @@
-pragma solidity ^0.5.0;
-pragma experimental ABIEncoderV2;
-
- //global idCarta
-
 contract Pokard {
 
     struct Pokemon{
+    address payable dono;
     uint idCarta;
     uint idPokemon; //reconhecer pela api
     uint ataque;
@@ -15,9 +11,12 @@ contract Pokard {
     
     address payable owner;
     address payable jogador; //endereço do jogador
-    mapping(address => Pokemon[]) PokemonsPorJogador; //cada endereço tera um array de pokemons
+    mapping(address => Pokemon[]) private PokemonsPorJogador; //cada endereço tera um array de pokemons
     mapping(address => Pokemon[]) PokemonsMercado;
-    uint256 valorCarta= 0.1 ether;
+    address payable [] jogadorPokemons;
+    Pokemon[] PokemonsMercado2; // vaicolocar o idDacarta
+     uint256 valorCarta= 0.1 ether;
+     uint idCarta = 0;
     
     
     constructor()public{  owner =  msg.sender; }
@@ -29,69 +28,71 @@ contract Pokard {
         require(msg.value==valorCarta*quant);
         for(uint i=0; i<quant; i++)
             {
-                gerarPokemon();
+                gerarPokemon(i, msg.sender);
                 //PokemonsPorJogador[msg.sender].push(gerarPokemon());
             }    
     }
 
     //sorteia um valor correspondente ao id do pokemon
-   function gerarPokemon() public
+   function gerarPokemon(uint aux, address payable endereco) private
    { 
         //Pokemon memory pokemonGerado;
+        uint ataque = aux+2;
+        uint defesa = aux;
         Pokemon memory novoPokemon;
-        novoPokemon.ataque = uint(keccak256(abi.encodePacked(now, msg.sender))) % 100;
-        novoPokemon.defesa = uint(keccak256(abi.encodePacked(now, msg.sender))) % 100;
-        novoPokemon.idPokemon = uint(keccak256(abi.encodePacked(now, msg.sender))) % 898;
-        PokemonsPorJogador[msg.sender].push(novoPokemon);
+        novoPokemon.idCarta = idCarta;
+        novoPokemon.dono = endereco;
+        novoPokemon.ataque = uint(keccak256(abi.encodePacked(now*ataque, msg.sender))) % 100;
+        novoPokemon.defesa = uint(keccak256(abi.encodePacked(now*defesa, msg.sender))) % 100;
+        novoPokemon.idPokemon = uint(keccak256(abi.encodePacked(now*aux, msg.sender))) % 898;
+        PokemonsPorJogador[endereco].push(novoPokemon);
+        //jogadorPokemons.push(msg.sender);
+        idCarta++;
       
    }
    
     //vai retornar todos os pokemons do jogador
    function retornaPokemonsComprados() public view returns (Pokemon[] memory)
-   {   // string memory retorno[];
-         //uint constant n = 32**22 + 8;
-        //uint [5] memory pokemons;
-       /* for(uint i=0; i<PokemonsPorJogador[msg.sender].length; i++){
-            uint valor = PokemonsPorJogador[msg.sender][i];
-            pokemons[i] = valor;
-           // pokemons.push(valor);
-          
-            
-        } */
-    
-        //string memory listaPokemons = pokemonToString(pokemons);
+   {   
+       
        
         return PokemonsPorJogador[msg.sender];
    }
 
-   /*function pokemonToString(Pokemon [] memory pokemons) private returns (string memory)
+   function mostrarPokemonsMercado() public view returns (Pokemon[] memory)
    {
-       string memory retorno;
-       for(uint i=0; i<PokemonsPorJogador[msg.sender].length; i++){
-           
-           uint a = pokemons[i].idCarta;
-           uint b = pokemons[i].ataque;
-           uint c = pokemons[i].forca;
-           string pokemonString = string(abi.encodePacked(",[",a, b, c, d, e, "],"));
-            
-            
-        }
-        return retorno;
-   }*/
+       return PokemonsMercado2;
+   }
 
+ 
    function colocarPokemonAvenda(uint id, uint preco) public{
 
        PokemonsPorJogador[msg.sender];
        for(uint i=0; i<PokemonsPorJogador[msg.sender].length; i++){
-            if( PokemonsPorJogador[msg.sender][i].idPokemon== id)
+            if( PokemonsPorJogador[msg.sender][i].idCarta== id)
             {
                 PokemonsPorJogador[msg.sender][i].preco = preco;
-                PokemonsMercado[msg.sender].push(PokemonsPorJogador[msg.sender][i]);
-                delete PokemonsPorJogador[msg.sender][i];
+                //PokemonsMercado[msg.sender].push(PokemonsPorJogador[msg.sender][i]);
+                PokemonsMercado2.push(PokemonsPorJogador[msg.sender][i]);
             }
         } 
 
        
    }
    
+   function comprarPokemonMercado( uint idCarta) public payable {
+      
+      for(uint i=0; i<PokemonsMercado2.length; i++){
+            if( PokemonsMercado2[i].idCarta== idCarta)
+            {
+                require(msg.value==PokemonsMercado2[i].preco);
+                address(PokemonsMercado2[i].dono).transfer(msg.value); //trenferir pro endereço vendedor
+                //PokemonsMercado[msg.sender].push(PokemonsPorJogador[msg.sender][i]);
+                delete PokemonsPorJogador[PokemonsMercado2[i].dono];
+                delete PokemonsMercado2[i];
+                break;
+            }
+        } 
+   }
+
 }
